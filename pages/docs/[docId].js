05e2f83cf53f608044b1eff0ceba3a16e1d1398a
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-31 22:21:56 +0530

    fixed before rending of editor page

--- pages/docs/[docId].js
+++ pages/docs/[docId].js
@@ -23,55 +23,57 @@ const DocId = () => {
 
   if (!session) return <LogIn />;
 
-  if (!loadingSnapshot && !snapshot?.data().fileName) {
+  if (!loadingSnapshot && !snapshot?.data()) {
     router.replace('/');
+    return <h1>Loading..</h1>;
   }
 
   return (
     <div>
-      <header className='flex justify-between items-center p-3'>
+      {snapshot?.data() && (<>
+        <header className='flex justify-between items-center p-3'>
         <span onClick={() => router.push('/')} className='cursor-pointer'>
         <Icon name='description' size='5xl' color='blue' />
         </span>
 
-        <div className='flex-grow px-2'>
-          <h2 className='text-xl font-semibold'>
-            {snapshot?.data().fileName}
-          </h2>
+          <div className='flex-grow px-2'>
+            <h2 className='text-xl font-semibold'>
+              {snapshot?.data().fileName}
+            </h2>
 
-          <div
-            className='flex items-center text-sm space-x-1
+            <div
+              className='flex items-center text-sm space-x-1
              -ml-1 h-8 text-gray-600'>
-            <p className='option-p'>File</p>
-            <p className='option-p'>Edit</p>
-            <p className='option-p'>View</p>
-            <p className='option-p'>Insert</p>
-            <p className='option-p'>Format</p>
-            <p className='option-p'>Tools</p>
+              <p className='option-p'>File</p>
+              <p className='option-p'>Edit</p>
+              <p className='option-p'>View</p>
+              <p className='option-p'>Insert</p>
+              <p className='option-p'>Format</p>
+              <p className='option-p'>Tools</p>
+            </div>
           </div>
-        </div>
 
-        <Button
-          color='lightBlue'
-          buttonType='filled'
-          size='regular'
-          rounded={false}
-          block={false}
-          iconOnly={false}
-          ripple='light'
-          className='hidden md:inline-flex h-10 rounded-sm'
-        >
-          <Icon name='people' size='md' /> Share
-        </Button>
+          <Button
+            color='lightBlue'
+            buttonType='filled'
+            size='regular'
+            rounded={false}
+            block={false}
+            iconOnly={false}
+            ripple='light'
+            className='hidden md:inline-flex h-10 rounded-sm'
+          >
+            <Icon name='people' size='md' /> Share
+          </Button>
 
-        <img
-          className='rounded-full cursor-pointer w-10 h-10 ml-2'
-          src={session.user.image}
-          alt='user profile'
-        />
-      </header>
-
-      {snapshot?.data() && <TextEditor editorData={snapshot?.data().editorState} />}
+          <img
+            className='rounded-full cursor-pointer w-10 h-10 ml-2'
+            src={session.user.image}
+            alt='user profile'
+          />
+        </header>
+        <TextEditor editorData={snapshot?.data().editorState} />
+      </>)}
     </div>
   );
 };
